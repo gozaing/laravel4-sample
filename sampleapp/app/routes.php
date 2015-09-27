b815f345987a1b638c550c5c4b1bc785020822ca
HideyasuTobaru <steganographi@gmail.com>
2015-09-27 21:32:07 +0900

    add sample routing and debugbar

--- sampleapp/app/routes.php
+++ sampleapp/app/routes.php
@@ -13,7 +13,55 @@
 
 Route::get('/', function()
 {
-	return View::make('hello');
+	return 'hello root';
+});
 
+Route::get('/hello', function()
+{
+    return 'Hello World';
+});
 
+Route::get('/magic', [
+    'as' => 'magical.route',
+    function()
+    {
+        return 'magical laravel';
+    }
+]);
+
+Route::get('/magic-link',
+    function()
+    {
+        return route('magical.route');
+    }
+);
+
+Route::get('/user/{name?}',
+    function($userName = '世界')
+{
+    return 'hello '. $userName. 'さん';
+})
+->where('name', '[A-Za-z]+');
+
+Route::get('text', function()
+{
+    Log::info('例：テキストを返す');
+    Log::warning('例：warningを返す');
+    Log::error('例：errorを返す');
+    Log::debug('debug test');
+    return 'お寿司';
+});
+
+Route::get('notfound', function()
+{
+    //App::abort(404);
+    App::abort(404);
+    //return 'お寿司';
+});
+
+Route::post('happy-birthday', function(){
+   return Response::json([
+       'result' => 'NG',
+       'message' => '引数"birthday_present"は必ず添付してください。',
+       ],400);
 });
